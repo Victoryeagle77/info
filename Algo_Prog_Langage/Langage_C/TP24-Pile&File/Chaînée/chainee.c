c2e4af48b1346f4ce56e3021ac2219eac4acfc18
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2019-03-20 23:01:24 +0100

    Update chainee.c

--- Algo_Prog_Langage/Langage_C/TP24-Pile&File/Chaînée/chainee.c
+++ Algo_Prog_Langage/Langage_C/TP24-Pile&File/Chaînée/chainee.c
@@ -1,5 +1,21 @@
 /* Question 3 : Chaînée. 
-Faite une file de caractère avec une des structure et un système de maillage.
+Faite une file de caractère avec une des structure et un système de maillage, 
+en vous aidant des indications suviantes :
+
+Nous allons avoir besoin de stocker des caractères dans une pile pour le troisième exercice. 
+Afin de préparer le terrain, nous allons réaliser une telle pile à l'aide d'une liste chaînée et la tester.
+
+Écrivez la définition d'un maillon, et la définition de trois fonctions qui représentent les opérations push, pop et empty. 
+Dans un autre fichier, écrivez un programme de test qui permet à son utilisateur de donner une suite d'ordres à une pile.
+
+Certaines piles supportent deux opérations supplémentaires : 
+top renvoie la donnée la plus récente mais ne la retire pas de la pile, 
+tandis que clear retire toutes les données de la pile et ne renvoie rien. 
+Ces opérations peuvent être vues comme des combinaisons des opérations basiques, 
+mais en leur donnant leur propre codage on peut en améliorer les performances.
+
+Ajoutez au programme du premier exercice deux fonctions qui représentent ces opérations. 
+Imaginez et réalisez de nouveaux ordres pour tester ces fonctions
 */
 
 #include <stdio.h>
@@ -9,8 +25,7 @@ Faite une file de caractère avec une des structure et un système de maillage.
 
 void main(void) {
 	file f;
-	char instru;
-	char stock;
+	char instru, stock;
 	f.premier=NULL;
 	f.dernier=NULL;
 	printf("La file attend vos ordres\n");
@@ -18,20 +33,20 @@ void main(void) {
 		printf("> ");
 		instru=getchar();
 		if (instru=='+') {
-			stock=getchar();
-			enqueue(&f,stock);
+			stock = getchar();
+			enqueue(&f, stock);
 			printf("Le caractère %c a été ajouté\n", stock);
 		}
 		if (empty(f))
 			printf("La file est vide\n");
 		else {
-			if (instru=='-') {
-				stock=dequeue(&f);
+			if (instru == '-') {
+				stock = dequeue(&f);
 				printf("Le caractère %c a été supprimé\n", stock);	
-			} else if (instru=='t') {
-				stock=top(f);
+			} else if (instru == 't') {
+				stock = top(f);
 				printf("Le caractère au sommet de la file est %c\n", stock);
-			} else if (instru=='c') {
+			} else if (instru == 'c') {
 				clear(&f);
 				printf("La file a été vidé\n");
 			}
