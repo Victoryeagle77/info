3439f3d707188c050ef5496685dba61d3f0142ed
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-30 10:55:14 +0000

    Update mandelbrot.c

--- Algo_Prog_Langage/Langage_C/TP21-libraires graphiques/Librairie-SDL/mandelbrot.c
+++ Algo_Prog_Langage/Langage_C/TP21-libraires graphiques/Librairie-SDL/mandelbrot.c
@@ -6,8 +6,7 @@ avec un mode caméra pour visualiser sous différent angles la figures. */
 #include <stdlib.h>
 #include <SDL/SDL.h>
 
-/* Mode de coloration 32 bit */
-static void aire(SDL_Surface *surface, int x, int y, Uint16 pixel){
+static void aire(SDL_Surface *surface, unsigned short int x, unsigned short int y, Uint16 pixel){
   volatile uint8_t octet = surface->format->BytesPerPixel;
   Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * octet;
   switch(octet){
@@ -17,6 +16,7 @@ static void aire(SDL_Surface *surface, int x, int y, Uint16 pixel){
       p[0] = (pixel >> 8);
       else { p[0] = pixel; }
       break;
+    /* Mode de défintion 8 bit */
     case 4: *(Uint16 *)p = pixel; break;
   }
 }
@@ -37,7 +37,7 @@ static void controle(double position, const unsigned short int *couleur, double 
         switch (evenement.key.keysym.sym){
           /* Flèche de gauche ==> bouge la caméra vers la gauche */
           case SDLK_LEFT: x -= 20/position; break;
-          /* Flèche de droite ==> bouge la caméra vers la droite */
+          /* Flèche de evenementdroite ==> bouge la caméra vers la droite */
           case SDLK_RIGHT: x += 20/position; break;
           /* Flèche du haut ==> bouge la caméra vers le haut */
           case SDLK_UP: y -= 20/position; break;
@@ -50,9 +50,11 @@ static void controle(double position, const unsigned short int *couleur, double 
           /* Touche espace ==> fait une rotation */
           case SDLK_SPACE: position *= -1; break;    
         }
-        long double camera = position/200;
-        long double x1 = x - (1.5/camera), x2 = x + 1.5/camera;
-        long double y1 = y - (1.2/camera), y2 = y + 1.2/camera;
+        /* Initialiser le niveau de déplacement de la caméra sur la fenetre */
+        volatile long double camera = position/200;
+        volatile long double x1 = x - (1.5/camera), x2 = x + 1.5/camera;
+        volatile long double y1 = y - (1.2/camera), y2 = y + 1.2/camera;
+        /* Afficahge verticale et horizontale après déplacement */
         volatile unsigned short int image_x = (x2 - x1) * position;
         volatile unsigned short int image_y = (y2 - y1) * position;
         for(unsigned short int x=0; x<image_x; x++){
@@ -60,14 +62,15 @@ static void controle(double position, const unsigned short int *couleur, double 
             long double camera_x = x / position + x1, 
             camera_y = y / position + y1, 
             complexe_x = 0, complexe_y = 0;
+            /* Indice de déplacement */
             int i = 0;
             for(; complexe_x*complexe_x + complexe_y*complexe_y <4 && i < max; i++){
               long double tmp = complexe_x;
               complexe_x = complexe_x*complexe_x - complexe_y*complexe_y + camera_x;
               complexe_y = 2*complexe_y*tmp + camera_y;
             }
-            if (i < max) { tab[x][y]=couleur[i%150]; }
-            else { tab[x][y]=0; }
+            if(i < max){ tab[x][y]=couleur[i%150]; }
+            else{ tab[x][y]=0; }
           }
         }
     }
@@ -75,8 +78,8 @@ static void controle(double position, const unsigned short int *couleur, double 
     actuel = SDL_GetTicks();
     if (actuel - precedent > 30){
       precedent = actuel;
-      for (int i = 0; i < 600; i++){
-        for (int j = 0; j < 480; j++)
+      for(unsigned short int i = 0; i < 600; i++){
+        for(unsigned short int j = 0; j < 480; j++)
           aire(ecran, i, j, tab[i][j]);
       }
     } else { SDL_Delay(30 - (actuel - precedent)); }
@@ -86,8 +89,8 @@ static void controle(double position, const unsigned short int *couleur, double 
 }
 
 static void coloration(unsigned short int *couleur){
-  unsigned short int rouge = 0, vert = 0, bleu = 0;
-  unsigned short int i = 0;
+  static unsigned short int rouge = 0, vert = 0, bleu = 0;
+  static unsigned short int i = 0;
   /* Luminosité et couleur de la coloration du fond, et de ses éléments */
   for(; i<25; i++){
     couleur[i] = rouge*256*256 + bleu*256 + vert;
@@ -104,18 +107,17 @@ static void coloration(unsigned short int *couleur){
 }
 
 void main(void){
-  unsigned short int rouge=0, vert=0, bleu=0;
-  unsigned short int i=0, couleur[150];
+  static unsigned short int couleur[150];
   double position = 200;
   double camera = position/200;
   double x1 = -1.5/camera, x2 = 1.5/camera;
   double y1 = -1.2/camera, y2 = 1.2/camera;
   double x = 0, y = 0;
     
-  int precedent = 0, actuel = 0, max = 150;
+  unsigned short int precedent = 0, actuel = 0, max = 150;
   int image_x = (x2 - x1) * position, image_y = (y2 - y1) * position;
   /* Luminosité de la coloration du fond */
- coloration(couleur);
+  coloration(couleur);
   static unsigned int tab[600][480];
   /* Coordonnées et dimensions de la figure sur l'écran */
   for(int x=0; x < image_x; x++){
@@ -137,6 +139,7 @@ void main(void){
   /* Initialise les composants d'écran à placer dans la fen^tre */
   SDL_Surface *ecran = NULL;
   SDL_Event evenement;
+  
   SDL_Init(SDL_INIT_VIDEO);
   ecran = SDL_SetVideoMode(600, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
   SDL_WM_SetCaption("Mandelbrot", NULL);
@@ -144,5 +147,6 @@ void main(void){
   controle(position, couleur, x, y, ecran, precedent, actuel, tab, max);
   /* Sortie */
   SDL_Quit();
+  
   exit(0);
 }
