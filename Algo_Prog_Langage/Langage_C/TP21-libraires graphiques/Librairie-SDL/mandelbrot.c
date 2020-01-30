178b283c80d4ae9c68cc6b01d8f21411948c4ea9
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-01-30 10:16:50 +0000

    Update mandelbrot.c

--- Algo_Prog_Langage/Langage_C/TP21-libraires graphiques/Librairie-SDL/mandelbrot.c
+++ Algo_Prog_Langage/Langage_C/TP21-libraires graphiques/Librairie-SDL/mandelbrot.c
@@ -7,14 +7,14 @@ avec un mode caméra pour visualiser sous différent angles la figures. */
 #include <SDL/SDL.h>
 
 /* Mode de coloration 32 bit */
-void aire(SDL_Surface *surface, int x, int y, Uint16 pixel){
-  int octet = surface->format->BytesPerPixel;
+static void aire(SDL_Surface *surface, int x, int y, Uint16 pixel){
+  volatile uint8_t octet = surface->format->BytesPerPixel;
   Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * octet;
   switch(octet){
     case 1: *p = pixel; break;
-    /* Décalage à droite par 16 en big endian */
+    /* Décalage à droite par 8 en big endian */
     case 3: if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
-      p[0] = (pixel >> 16);
+      p[0] = (pixel >> 8);
       else { p[0] = pixel; }
       break;
     case 4: *(Uint16 *)p = pixel; break;
@@ -22,10 +22,10 @@ void aire(SDL_Surface *surface, int x, int y, Uint16 pixel){
 }
 
 /* Gestion de controle des touches sur la fractale */
-void controle(double position, int couleur[150], double x, double y,
+static void controle(double position, int couleur[150], double x, double y,
               SDL_Surface *ecran, int precedent, int actuel, unsigned int tab[600][480], int max){
-  SDL_Event evenement;
-  int continuer = 1;
+  static SDL_Event evenement;
+  static volatile unsigned short int continuer = 1;
   while (continuer){
     /* Remplir la forme avec les paramètre evénementielle*/
     SDL_PollEvent(&evenement);
@@ -53,10 +53,10 @@ void controle(double position, int couleur[150], double x, double y,
         long double camera = position/200;
         long double x1 = x - (1.5/camera), x2 = x + 1.5/camera;
         long double y1 = y - (1.2/camera), y2 = y + 1.2/camera;
-        int image_x = (x2 - x1) * position;
-        int image_y = (y2 - y1) * position;
-        for(int x=0; x<image_x; x++){
-          for(int y=0; y<image_y; y++){
+        volatile unsigned short int image_x = (x2 - x1) * position;
+        volatile unsigned short int image_y = (y2 - y1) * position;
+        for(unsigned short int x=0; x<image_x; x++){
+          for(unsigned short int y=0; y<image_y; y++){
             long double camera_x = x / position + x1, 
             camera_y = y / position + y1, 
             complexe_x = 0, complexe_y = 0;
@@ -85,15 +85,15 @@ void controle(double position, int couleur[150], double x, double y,
   }
 }
 
-void main(void){
-  int rouge=250, vert=0, bleu=0;
+void coloration(void){
+  unsigned short int rouge=0, vert=0, bleu=0;
   int i=0, couleur[150];
   double position = 200;
   double camera = position/200;
   double x1 = -1.5/camera, x2 = 1.5/camera;
   double y1 = -1.2/camera, y2 = 1.2/camera;
   double x = 0, y = 0;
-  /* Initialise les composants d'écran à placer dans la fen^tre */
+  /* Initialise les composants d'écran à placer dans la fenetre */
   SDL_Surface *ecran = NULL;
   SDL_Event evenement;
     
@@ -105,17 +105,14 @@ void main(void){
     vert+=10;
   }
   for (; i<50; i++){
-    couleur[i] = rouge*256*256 + bleu*256 + vert;
+    couleur[i] = rouge*256*256 + bleu*256  + vert;
     bleu+=10;
   }
   for (; i<75; i++){
     couleur[i] = rouge*256*256 + bleu*256 + vert;
     rouge+=10;
   }
-  SDL_Init(SDL_INIT_VIDEO);
-  ecran = SDL_SetVideoMode(600, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-  SDL_WM_SetCaption("Mandelbrot", NULL);
-  unsigned int tab[600][480];
+  unsigned int tab[100][100];
   /* Coordonnées et dimensions de la figure sur l'écran */
   for(int x=0; x < image_x; x++){
     for(int y=0; y < image_y; y++){
@@ -124,7 +121,7 @@ void main(void){
       double camera_y = y / position + y1;
       double complexe_x = 0;
       double complexe_y = 0;
-      int i = 0;
+      unsigned short int i = 0;
       for(; complexe_x*complexe_x + complexe_y*complexe_y <4 && i < max; i++){
         double tmp = complexe_x;
         complexe_x = complexe_x*complexe_x - complexe_y*complexe_y + camera_x;
@@ -133,6 +130,56 @@ void main(void){
       if(i < max){ tab[x][y] = couleur[i%150]; }
     }
   }
+}
+
+void main(void){
+  unsigned short int rouge=0, vert=0, bleu=0;
+  int i=0, couleur[150];
+  double position = 200;
+  double camera = position/200;
+  double x1 = -1.5/camera, x2 = 1.5/camera;
+  double y1 = -1.2/camera, y2 = 1.2/camera;
+  double x = 0, y = 0;
+    
+  int precedent = 0, actuel = 0, max = 150;
+  int image_x = (x2 - x1) * position, image_y = (y2 - y1) * position;
+  /* Luminosité de la coloration du fond */
+  for (; i<25; i++){
+    couleur[i] = rouge*256*256 + bleu*256 + vert;
+    vert+=10;
+  }
+  for (; i<50; i++){
+    couleur[i] = rouge*256*256 + bleu*256  + vert;
+    bleu+=10;
+  }
+  for (; i<75; i++){
+    couleur[i] = rouge*256*256 + bleu*256 + vert;
+    rouge+=10;
+  }
+  static unsigned int tab[600][480];
+  /* Coordonnées et dimensions de la figure sur l'écran */
+  for(int x=0; x < image_x; x++){
+    for(int y=0; y < image_y; y++){
+      /* Déplacement de l'image par rapport au déplacement de la caméra */
+      double camera_x = x / position + x1;
+      double camera_y = y / position + y1;
+      double complexe_x = 0;
+      double complexe_y = 0;
+      unsigned short int i = 0;
+      for(; complexe_x*complexe_x + complexe_y*complexe_y <4 && i < max; i++){
+        double tmp = complexe_x;
+        complexe_x = complexe_x*complexe_x - complexe_y*complexe_y + camera_x;
+        complexe_y = 2*complexe_y*tmp + camera_y;
+      }
+      if(i < max){ tab[x][y] = couleur[i%150]; }
+    }
+  }
+  /* Initialise les composants d'écran à placer dans la fen^tre */
+  SDL_Surface *ecran = NULL;
+  SDL_Event evenement;
+  SDL_Init(SDL_INIT_VIDEO);
+  ecran = SDL_SetVideoMode(600, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+  SDL_WM_SetCaption("Mandelbrot", NULL);
   SDL_EnableKeyRepeat(3, 3);
   controle(position, couleur, x, y, ecran, precedent, actuel, tab, max);
   /* Sortie */
