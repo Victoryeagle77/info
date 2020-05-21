5ebd935517d23cb63a1775f5b0e887c1e5a0d205
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-05-22 00:18:09 +0200

    Update interface.h

--- Projets/(Projet_C) - Miroir_Magique/header/interface.h
+++ Projets/(Projet_C) - Miroir_Magique/header/interface.h
@@ -9,6 +9,26 @@
 #include <X11/Xutil.h>
 #include <X11/Xresource.h>
 
+volatile typedef void (*Reception)(void *data);
+
+/* Structure definissant un bouton */
+typedef struct Button Button;
+struct Button{
+  XChar2b *text;
+  volatile unsigned short int text_width,
+    font_ascent, width, height;
+  volatile unsigned long border, background, foreground;
+  void *data;
+  volatile Reception buttonRelease;
+};
+
+/* Structure definissant les actions de sortie */
+typedef struct sortie Sortie;
+struct sortie {
+  Display *d;
+  XFontStruct *font;
+};
+
 extern void menu(void);
 
 #endif
