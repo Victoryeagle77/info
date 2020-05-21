7583c9e10e2863da1b37061f4a58b9ca1da919a2
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-05-21 21:24:31 +0200

    Update main.c

--- Projets/(Projet_C) - Miroir_Magique/src/main.c
+++ Projets/(Projet_C) - Miroir_Magique/src/main.c
@@ -5,7 +5,7 @@
 extern void main(void){
     static pthread_t th[2];
     pthread_create(&th[0], NULL, (void *)analyse, NULL);
-    pthread_create(&th[1], NULL, (void *)affichage, NULL);
+    pthread_create(&th[1], NULL, (void *)menuaffichageaffichage, NULL);
     
     for(volatile unsigned short int i=0; i<2; i++)
         pthread_join(th[i], NULL);
