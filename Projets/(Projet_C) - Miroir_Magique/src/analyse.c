1c36b7609c0dc8367bfd49357ffc3597604c2707
Victoryeagle77 <44432686+Victoryeagle77@users.noreply.github.com>
2020-06-08 20:41:18 +0200

    Update analyse.c

--- Projets/(Projet_C) - Miroir_Magique/src/analyse.c
+++ Projets/(Projet_C) - Miroir_Magique/src/analyse.c
@@ -11,8 +11,8 @@ extern volatile uint8_t donnee[5];
 * et la configuration d'utilisation du DHT11.
 */
 static void configuration(void){
-	/* Ecrire sur le pin en sortie */
-	pinMode(GPIO, OUTPUT);
+    /* Ecrire sur le pin en sortie */
+    pinMode(GPIO, OUTPUT);
 
     /* On effectue un front montant pour configurer les signaux logiques.
         |-----|
@@ -27,7 +27,7 @@ static void configuration(void){
     usleep(4);
 
     /* Lire le pin en entree */
-	pinMode(GPIO, INPUT);
+    pinMode(GPIO, INPUT);
 }
 
 /**
@@ -36,50 +36,50 @@ static void configuration(void){
 * par operations bianires.
 */
 static void lire_donnee(void){
-	volatile uint_fast8_t tmp = 1, decalage = 0, j = 0;
+    volatile uint_fast8_t tmp = 1, decalage = 0, j = 0;
+    /* protocole */
+    configuration();
 
-	configuration();
+    /* Lire les donnees lors de la detection d'un changement */
+    for(volatile unsigned short i=0; i<85; i++){
+        decalage = 0;
+        while(digitalRead(GPIO) == tmp) {
+            decalage++;
+            delayMicroseconds(1);
+            /* En cas d'atteinte la limite de 2^8 valeurs, soit 255 valeurs,
+            L'incrementation du decalage est remise a 0. */
+            if(decalage == 255){ break; }
+        }
 
-	/* Lire les donnees lors de la detection d'un changement */
-	for(volatile unsigned short i=0; i<85; i++){
-		decalage = 0;
-		while(digitalRead(GPIO) == tmp) {
-			decalage++;
-			delayMicroseconds(1);
-			/* En cas d'atteinte la limite de 2^8 valeurs, soit 255 valeurs,
-			L'incrementation du decalage est remise a 0. */
-			if(decalage == 255){ break; }
-		}
+        /* Lecture de l'etat du signal logique du GPIO */
+        tmp = digitalRead(GPIO);
+        /* En cas d'atteinte la limite de 2^8 valeurs, soit 255 valeurs,
+        L'incrementation du decalage est remise a 0. */
+        if(decalage == 255){ break; }
 
-		/* Lecture de l'etat du signal logique du GPIO */
-		tmp = digitalRead(GPIO);
-		/* En cas d'atteinte la limite de 2^8 valeurs, soit 255 valeurs,
-		L'incrementation du decalage est remise a 0. */
-		if(decalage == 255){ break; }
+        if((i >= 4) && (i%2 == 0)){
+            /* On stocke chaque bit dans un octet de recuperation,
+            par decalage bit a bit. */
+            donnee[j/8] <<= 1;
+            if(decalage > 50){ donnee[j/8] |= 1; }
+            j++;
+        }
+    }
 
-		if((i >= 4) && (i%2 == 0)){
-			/* On stocke chaque bit dans un octet de recuperation,
-			par decalage bit a bit. */
-			donnee[j/8] <<= 1;
-			if(decalage > 50){ donnee[j/8] |= 1; }
-			j++;
-		}
-	}
-
-	/* Variable tampont recueprant les valeurs correctes */
-	static volatile uint8_t valeur[5] = {0};
-	/* Verifier que la somme du dernier octet et que la lecture est de 40 bits.
-	(car on a 8 bits (1 octet) * 5 cases du tableau de donnees = 40),
-	On s'assure ainsi que cela respecte les bornes de mesure de la sonde. */
-	if((j >= 40) && (donnee[4] == (donnee[0] + 
-			donnee[1] + donnee[2] + donnee[3]))){
-		for(volatile unsigned short i=0; i<5; i++)
-			valeur[i] = donnee[i];
-	}else{
-		/* Si la valeur est incorrecte, on prend la precedente. */
-		for(volatile unsigned short i=0; i<5; i++)
-			donnee[i] = valeur[i];
-	}
+    /* Variable tampont recueprant les valeurs correctes */
+    static volatile uint8_t valeur[5] = {0};
+    /* Verifier que la somme du dernier octet et que la lecture est de 40 bits.
+    (car on a 8 bits (1 octet) * 5 cases du tableau de donnees = 40),
+    On s'assure ainsi que cela respecte les bornes de mesure de la sonde. */
+    if((j >= 40) && (donnee[4] == (donnee[0] + 
+        donnee[1] + donnee[2] + donnee[3]))){
+        for(volatile unsigned short i=0; i<5; i++)
+            valeur[i] = donnee[i];
+    }else{
+        /* Si la valeur est incorrecte, on prend la precedente. */
+        for(volatile unsigned short i=0; i<5; i++)
+            donnee[i] = valeur[i];
+    }
 }
 
 /**
@@ -88,11 +88,11 @@ static void lire_donnee(void){
 * et la deploiement de la librairie wiringPi.
 */
 extern void analyse(void){
-	/* Deploiement de la librairie wiringPi */
-	if(wiringPiSetup() == -1){ exit(1); }
-	/* Emettre continuellement toutes les secondes */
-	while(1){
-		lire_donnee();
-		sleep(1);
-	}
+    /* Deploiement de la librairie wiringPi */
+    if(wiringPiSetup() == -1){ exit(1); }
+    /* Emettre continuellement toutes les secondes */
+    while(1){
+        lire_donnee();
+        sleep(1);
+    }
 }
