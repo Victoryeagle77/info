11fa1a8854a18b23755989c1c39b66090af8be3b
CHENU <Victoryeagle77@gmail.com>
2020-06-08 13:19:24 +0200

    commit

---
+++ Projets/(Projet_C) - Miroir_Magique/src/analyse.c
@@ -0,0 +1,98 @@
+#include "../header/analyse.h"
+
+#define GPIO 7
+
+extern volatile uint8_t donnee[5];
+
+/**
+* @function configuration
+* Permet d'effectuer une transitions sur les niveaux logiques,
+* pour définir le protocole (le temps d'attente entre l'ecriture et la lecture de donnees),
+* et la configuration d'utilisation du DHT11.
+*/
+static void configuration(void){
+	/* Ecrire sur le pin en sortie */
+	pinMode(GPIO, OUTPUT);
+
+    /* On effectue un front montant pour configurer les signaux logiques.
+        |-----|
+        |  1  | (Etat haut du signal logique)
+        A     V
+        |  0  | (Etat bas du signal logique)
+    ----|     |-----
+    */
+    digitalWrite(GPIO, 0);
+    usleep(20000);
+    digitalWrite(GPIO, 1);
+    usleep(4);
+
+    /* Lire le pin en entree */
+	pinMode(GPIO, INPUT);
+}
+
+/**
+* @function lire_donnee
+* Permet de lire les donnees du capteur dht11,
+* par operations bianires.
+*/
+static void lire_donnee(void){
+	volatile uint_fast8_t tmp = 1, decalage = 0, j = 0;
+
+	configuration();
+
+	/* Lire les donnees lors de la detection d'un changement */
+	for(volatile unsigned short i=0; i<85; i++){
+		decalage = 0;
+		while(digitalRead(GPIO) == tmp) {
+			decalage++;
+			delayMicroseconds(1);
+			/* En cas d'atteinte la limite de 2^8 valeurs, soit 255 valeurs,
+			L'incrementation du decalage est remise a 0. */
+			if(decalage == 255){ break; }
+		}
+
+		/* Lecture de l'etat du signal logique du GPIO */
+		tmp = digitalRead(GPIO);
+		/* En cas d'atteinte la limite de 2^8 valeurs, soit 255 valeurs,
+		L'incrementation du decalage est remise a 0. */
+		if(decalage == 255){ break; }
+
+		if((i >= 4) && (i%2 == 0)){
+			/* On stocke chaque bit dans un octet de recuperation,
+			par decalage bit a bit. */
+			donnee[j/8] <<= 1;
+			if(decalage > 50){ donnee[j/8] |= 1; }
+			j++;
+		}
+	}
+
+	/* Variable tampont recueprant les valeurs correctes */
+	static volatile uint8_t valeur[5] = {0};
+	/* Verifier que la somme du dernier octet et que la lecture est de 40 bits.
+	(car on a 8 bits (1 octet) * 5 cases du tableau de donnees = 40),
+	On s'assure ainsi que cela respecte les bornes de mesure de la sonde. */
+	if((j >= 40) && (donnee[4] == (donnee[0] + 
+			donnee[1] + donnee[2] + donnee[3]))){
+		for(volatile unsigned short i=0; i<5; i++)
+			valeur[i] = donnee[i];
+	}else{
+		/* Si la valeur est incorrecte, on prend la precedente. */
+		for(volatile unsigned short i=0; i<5; i++)
+			donnee[i] = valeur[i];
+	}
+}
+
+/**
+* @function analyse
+* Permet la lecture des donnees toutes les secondes;
+* et la deploiement de la librairie wiringPi.
+*/
+extern void analyse(void){
+	/* Deploiement de la librairie wiringPi */
+	if(wiringPiSetup() == -1){ exit(1); }
+	/* Emettre continuellement toutes les secondes */
+	while(1){
+		lire_donnee();
+		sleep(1);
+	}
+}
---
+++ Projets/(Projet_C) - Miroir_Magique/src/utilitaire.c
@@ -0,0 +1,48 @@
+#include "../header/interface.h"
+
+/**
+* @function *date
+* Permet de relever la date actuelle :
+* Annee, moi, jour, heures et minutes.
+* Le tout est converti en chaine de caractere dans :
+* @param str : La chaine recuperant la date
+* @return str : La chaine convertie
+*/
+extern volatile unsigned char *date(char *str){
+    time_t temps = time(NULL);
+    volatile struct tm tm = *localtime(&temps);
+    sprintf(str, "%02d/%02d/%d - %02d:%02d", tm.tm_mday, 
+                tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min);
+    return str;
+}
+
+extern const uint_fast8_t encodage(const XChar2b *texte){
+   static volatile uint_fast8_t i=0;
+   for(i=0; (texte[i].byte1) || (texte[i].byte2); i++);
+   return i;
+}
+
+extern const uint_fast8_t decodage(volatile XChar2b *affiche, 
+                                    const uint_fast8_t debordement, 
+                                    const char *saisie, const uint_fast8_t chaine){
+   static volatile uint_fast8_t i, j;
+   for(i=0, j=0; (i<chaine) && (j<debordement); i++){
+      if(saisie[i] < 128){
+         affiche[j].byte1 = 0;
+         affiche[j].byte2 = saisie[i]; 
+         j++;
+      }
+   }
+   return j;
+}
+
+extern void quitter(void *data){
+   Sortie *e = (Sortie*)data;
+   /* Fermeture du flux generant la police */
+   XFreeFont(e->d, e->font);
+   /* Destruction de tous les elements */
+   DestroyAll;
+   /* Fermeture des fenetres */
+   XCloseDisplay(e->d);
+   exit(0);
+}
